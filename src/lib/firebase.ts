cd1a6665163c99eb85366154a83f3032b005fabf
adel <125857364+adelineafgr@users.noreply.github.com>
2026-08-07 15:23:45 +0700

    Add files via upload

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const config = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId
+};
+
+const app = getApps().length === 0 ? initializeApp(config) : getApp();
+
+export const db = firebaseConfig.firestoreDatabaseId && firebaseConfig.firestoreDatabaseId !== '(default)'
+  ? getFirestore(app, firebaseConfig.firestoreDatabaseId)
+  : getFirestore(app);
